7b9e806a06a14b3daf42c62a720722dd1facdf52
Jonathan Carnos <jonathan.carnos@gmail.com>
2021-03-23 17:42:02 +0100

    add feathers client

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
 } from "react-router-dom";
 import Cookie from "js-cookie";
 import Header from "./components/Header";
@@ -16,126 +16,183 @@ import Publish from "./containers/Publish/index";
 import Payment from "./containers/Payment/index";
 import "./App.css";
 import "./reset.css";
+import io from "socket.io-client";
+import feathers from "@feathersjs/client";
+import Axios from "axios";
+
+// Establish a Socket.io connection
+const socket = io("http://localhost:3030");
+
+// Initialize our Feathers client application through Socket.io
+// with hooks and authentication.
+const client = feathers();
+client.configure(feathers.socketio(socket));
+
+// Use localStorage to store our login token
+client.configure(
+  feathers.authentication({
+    storage: window.localStorage,
+  })
+);
 
 function App() {
-    const [token, setToken] = useState(Number(Cookie.get("token") || 0));
-    const [displayModalLogin, setDisplayModalLogin] = useState(false);
-    const [displayModalSignup, setDisplayModalSignup] = useState(false);
-    const [inputTitle, setInputTitle] = useState("");
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [publishClicked, setPublishClicked] = useState(false);
-    const [purchaseClicked, setPurchaseClicked] = useState(false);
-    const [details, setDetails] = useState({ offer: {} });
+  const [token, setToken] = useState(localStorage.getItem("feathers-jwt") || 0);
+  const [displayModalLogin, setDisplayModalLogin] = useState(false);
+  const [displayModalSignup, setDisplayModalSignup] = useState(false);
+  const [inputTitle, setInputTitle] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [publishClicked, setPublishClicked] = useState(false);
+  const [purchaseClicked, setPurchaseClicked] = useState(false);
+  const [details, setDetails] = useState({ offer: {} });
 
-    // Function to connect or disconnect the user
-    const setUser = (cookieToken) => {
-        if (cookieToken) {
-            Cookie.set("token", cookieToken, { expires: 10 });
-            setToken(cookieToken);
-        } else {
-            Cookie.remove("token");
-            setToken(null);
+  // Function to connect or disconnect the user
+  const setUser = (cookieToken) => {
+    if (cookieToken) {
+      Cookie.set("token", cookieToken, { expires: 10 });
+      setToken(cookieToken);
+    } else {
+      Cookie.remove("token");
+      setToken(null);
+    }
+  };
+
+  //   const authenticate = async (strategy, email, password) => {
+  //       return await client.auth
+  //   }
+
+  const login = async () => {
+    try {
+      // First try to log in with an existing JWT
+      const { accessToken } = await client.reAuthenticate();
+      setLoggedIn(true);
+      setToken(accessToken);
+    } catch (error) {
+      // If that errors, log in with email/password
+      // Here we would normally show a login page
+      // to get the login information
+      await setDisplayModalLogin(true);
+      //   return await client.authenticate({
+      //     strategy: "local",
+      //     email: "hello@feathersjs.com",
+      //     password: "supersecret",
+      //   });
+    }
+  };
+
+  useEffect(() => {
+    login();
+  }, []);
+
+  useEffect(() => {
+    if (!loggedIn && publishClicked) {
+      setDisplayModalLogin(true);
+    }
+    if (!loggedIn && purchaseClicked) {
+      setDisplayModalLogin(true);
+    }
+  }, [publishClicked, purchaseClicked]);
+
+  return (
+    <Router>
+      <Header
+        token={token}
+        setUser={setUser}
+        setDisplayModalLogin={setDisplayModalLogin}
+        setDisplayModalSignup={setDisplayModalSignup}
+        setInputTitle={setInputTitle}
+        setLoggedIn={setLoggedIn}
+        setPublishClicked={setPublishClicked}
+        setDetails={setDetails}
+      />
+
+      {/* Système d'affichage d'une modal pour le login et le signup */}
+      {displayModalLogin ? (
+        <Login
+          display={displayModalLogin}
+          setDisplay={setDisplayModalLogin}
+          setUser={setUser}
+          setToken={setToken}
+          setLoggedIn={setLoggedIn}
+          publishClicked={publishClicked}
+          setPublishClicked={setPublishClicked}
+          purchaseClicked={purchaseClicked}
+          setPurchaseClicked={setPurchaseClicked}
+          details={details}
+          setDetails={setDetails}
+        ></Login>
+      ) : null}
+      {displayModalSignup ? (
+        <Signup
+          display={displayModalSignup}
+          setDisplay={setDisplayModalSignup}
+          setUser={setUser}
+          setLoggedIn={setLoggedIn}
+        ></Signup>
+      ) : null}
+      {/* ------------------------ */}
+
+      {/* Verification that user is logged in before redirection */}
+      {purchaseClicked && loggedIn ? (
+        <Redirect
+          to={{
+            pathname: "/payment",
+            state: { afterPurchaseClickedAndLogin: true },
+          }}
+        />
+      ) : (
+        () => {
+          return setDisplayModalLogin(true);
         }
-    };
-
-    useEffect(() => {
-        if (!loggedIn && publishClicked) {
-            setDisplayModalLogin(true);
+      )}
+      {publishClicked && loggedIn ? (
+        <Redirect
+          to={{
+            pathname: "/publish",
+            state: { afterPublishClickedAndLogin: true },
+          }}
+        />
+      ) : (
+        () => {
+          return setDisplayModalLogin(true);
         }
-        if (!loggedIn && purchaseClicked) {
-            setDisplayModalLogin(true);
-        }
-    }, [publishClicked, purchaseClicked]);
+      )}
+      {/* ------------------- */}
 
-    return (
-        <Router>
-            <Header
-                token={token}
-                setUser={setUser}
-                setDisplayModalLogin={setDisplayModalLogin}
-                setDisplayModalSignup={setDisplayModalSignup}
-                setInputTitle={setInputTitle}
-                setLoggedIn={setLoggedIn}
-                setPublishClicked={setPublishClicked}
-                setDetails={setDetails}
-            />
-
-            {/* Système d'affichage d'une modal pour le login et le signup */}
-            {displayModalLogin ? (
-                <Login
-                    display={displayModalLogin}
-                    setDisplay={setDisplayModalLogin}
-                    setUser={setUser}
-                    setLoggedIn={setLoggedIn}
-                    publishClicked={publishClicked}
-                    setPublishClicked={setPublishClicked}
-                    purchaseClicked={purchaseClicked}
-                    setPurchaseClicked={setPurchaseClicked}
-                    details={details}
-                    setDetails={setDetails}
-                ></Login>
-            ) : null}
-            {displayModalSignup ? (
-                <Signup
-                    display={displayModalSignup}
-                    setDisplay={setDisplayModalSignup}
-                    setUser={setUser}
-                    setLoggedIn={setLoggedIn}
-                ></Signup>
-            ) : null}
-            {/* ------------------------ */}
-
-            {/* Verification that user is logged in before redirection */}
-            {purchaseClicked && loggedIn ? (
-                <Redirect
-                    to={{
-                        pathname: "/payment",
-                        state: { afterPurchaseClickedAndLogin: true },
-                    }}
-                />
-            ) : null}
-            {publishClicked && loggedIn ? (
-                <Redirect
-                    to={{
-                        pathname: "/publish",
-                        state: { afterPublishClickedAndLogin: true },
-                    }}
-                />
-            ) : null}
-            {/* ------------------- */}
-
-            <Switch>
-                <Route path="/offer/:id">
-                    <Offer
-                        setPurchaseClicked={setPurchaseClicked}
-                        token={token}
-                        details={details}
-                        setDetails={setDetails}
-                    />
-                </Route>
-                <Route path="/login"></Route>
-                <Route path="/signup"></Route>
-                <Route path="/publish">
-                    <Publish
-                        token={token}
-                        setPublishClicked={setPublishClicked}
-                    />
-                </Route>
-                <Route path="/payment">
-                    <Payment
-                        details={details}
-                        setDetails={setDetails}
-                        purchaseClicked={purchaseClicked}
-                        setPurchaseClicked={setPurchaseClicked}
-                    />
-                </Route>
-                <Route path="/">
-                    <Home inputTitle={inputTitle} />
-                </Route>
-            </Switch>
-            <Footer />
-        </Router>
-    );
+      <Switch>
+        <Route path="/offer/:id">
+          <Offer
+            setPurchaseClicked={setPurchaseClicked}
+            token={token}
+            details={details}
+            setDetails={setDetails}
+          />
+        </Route>
+        {/* <Route path="/login"></Route>
+        <Route path="/signup"></Route> */}
+        <Route path="/publish">
+          {(arg) => {
+            if (token) {
+              return (
+                <Publish token={token} setPublishClicked={setPublishClicked} />
+              );
+            }
+          }}
+        </Route>
+        <Route path="/payment">
+          <Payment
+            details={details}
+            setDetails={setDetails}
+            purchaseClicked={purchaseClicked}
+            setPurchaseClicked={setPurchaseClicked}
+          />
+        </Route>
+        <Route path="/">
+          <Home inputTitle={inputTitle} />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -3,112 +3,155 @@ import axios from "axios";
 import "./index.css";
 import "antd/dist/antd.css";
 import { message } from "antd";
+import io from "socket.io-client";
+import feathers from "@feathersjs/client";
+
+// Establish a Socket.io connection
+const socket = io("http://localhost:3030");
+
+// Initialize our Feathers client application through Socket.io
+// with hooks and authentication.
+const client = feathers();
+client.configure(feathers.socketio(socket));
+
+// Use localStorage to store our login token
+client.configure(
+  feathers.authentication({
+    storage: window.localStorage,
+  })
+);
 
 const Login = ({
-    setUser,
-    display,
-    setDisplay,
-    setLoggedIn,
-    setPublishClicked,
-    setPurchaseClicked,
-    details,
-    setDetails,
+  setUser,
+  setToken,
+  display,
+  setDisplay,
+  setLoggedIn,
+  setPublishClicked,
+  setPurchaseClicked,
+  details,
+  setDetails,
 }) => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await axios.post(
-                "https://lereacteurvinted.herokuapp.com/user/login",
-                { email, password }
-            );
-            if (response.status !== 200) {
-                console.log(response.data);
-            } else {
-                const token = response.data.token;
-                const copyDetails = Object.assign({}, details);
-                const newDetails = Object.assign(copyDetails, {
-                    user: response.data,
-                });
-                setDetails(newDetails);
-                setLoggedIn(true);
-                setUser(token);
-                setDisplay(false);
-            }
-        } catch (error) {
-            switch (error.response.data.error.message) {
-                case "Missing credentials":
-                    message.error(error.response.data.error.message);
-                    break;
-                case "No user exist with this email address":
-                    message.error(error.response.data.error.message);
-                    break;
-                case "Unauthorized":
-                    message.error(error.response.data.error.message);
-                    break;
-                default:
-                    console.log("an error occured");
-            }
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { accessToken } = await client.authenticate({
+        strategy: "local",
+        email,
+        password,
+      });
+      setLoggedIn(true);
+      setToken(accessToken);
+      setDisplay(false);
+    } catch (error) {
+      console.log(error);
+      //   switch (error.response.data.error.message) {
+      //     case "Missing credentials":
+      //       message.error(error.response.data.error.message);
+      //       break;
+      //     case "No user exist with this email address":
+      //       message.error(error.response.data.error.message);
+      //       break;
+      //     case "Unauthorized":
+      //       message.error(error.response.data.error.message);
+      //       break;
+      //     default:
+      //       console.log("an error occured");
+      //   }
+    }
+    // try {
+    //   const response = await axios.post(
+    //     "https://lereacteurvinted.herokuapp.com/user/login",
+    //     { email, password }
+    //   );
+    //   if (response.status !== 200) {
+    //     console.log(response.data);
+    //   } else {
+    //     const token = response.data.token;
+    //     const copyDetails = Object.assign({}, details);
+    //     const newDetails = Object.assign(copyDetails, {
+    //       user: response.data,
+    //     });
+    //     setDetails(newDetails);
+    //     setLoggedIn(true);
+    //     setUser(token);
+    //     setDisplay(false);
+    //   }
+    // } catch (error) {
+    //   switch (error.response.data.error.message) {
+    //     case "Missing credentials":
+    //       message.error(error.response.data.error.message);
+    //       break;
+    //     case "No user exist with this email address":
+    //       message.error(error.response.data.error.message);
+    //       break;
+    //     case "Unauthorized":
+    //       message.error(error.response.data.error.message);
+    //       break;
+    //     default:
+    //       console.log("an error occured");
+    //   }
+    // }
+  };
 
-    return (
-        <div
-            id="modal"
-            className={display ? "modalOn" : "modalOff"}
-            onClick={() => {
-                setDisplay(false);
-                setPublishClicked(false);
-                setPurchaseClicked(false);
-            }}
+  return (
+    <div
+      id="modal"
+      className={display ? "modalOn" : "modalOff"}
+      onClick={() => {
+        setDisplay(false);
+        setPublishClicked(false);
+        setPurchaseClicked(false);
+      }}
+    >
+      <div
+        id="modal-popup"
+        className="modal-login-popup"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
         >
-            <div
-                id="modal-popup"
-                className="modal-login-popup"
-                onClick={(e) => {
-                    e.stopPropagation();
-                }}
-            >
-                <form
-                    onSubmit={(e) => {
-                        handleSubmit(e);
-                    }}
-                >
-                    <h2
-                        style={{ left: "110px", marginBottom: "10px" }}
-                        className="h2-signup"
-                    >
-                        Connect to your account
-                    </h2>
-                    <label htmlFor="email">Email</label>
-                    <input
-                        name="email"
-                        type="email"
-                        placeholder="Enter your login email"
-                        value={email}
-                        onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}
-                    />
-                    <label htmlFor="password">Password</label>
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder="Enter your password"
-                        value={password}
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}
-                    />
-                    <button type="submit" className="button-login">
-                        Connectez-vous
-                    </button>
-                </form>
-            </div>
-        </div>
-    );
+          <h2
+            style={{ left: "110px", marginBottom: "10px" }}
+            className="h2-signup"
+          >
+            Connect to your account
+          </h2>
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="Enter your login email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value.toLowerCase());
+            }}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <button type="submit" className="button-login">
+            Connectez-vous
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
